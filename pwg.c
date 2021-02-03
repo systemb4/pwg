796ce3d5b84de33a52ea29884edce8a3b8c2026d
systemb4 <lukasnitzsche@yahoo.com>
2021-02-03 08:14:43 -0600

    move charsets to it's own file

--- pwg.c
+++ pwg.c
@@ -2,16 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "charsets.h"
+
 int main() {
     int begLength = 0;
     int endLength = 62;
     int spaceNum = 0;
-
-    char charset[62] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 
-        'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', 
-        '6', '7', '8', '9', '0'};
 
     srand(time(0)); 
     for(int numConOne = 0; numConOne < 30; numConOne++) {
