077a4754c858cdd7d6265b9c6d7ad4e5fcaaa496
systemb4 <lukasnitzsche@yahoo.com>
2021-02-02 19:08:00 -0600

    initial commit and edit readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pwg
-random password generator
+## random password generator
+A completely random password manager useful for passwords you don't want to rememeber
+
+---
+
+#### example
+BF5b58dlBUI9 AIKVpNzgbmkF OFgZIMj0fd90 90iyRQugn32C FoGGAQ0ckgZS
+Q9PW0LT7I3up GOtRGkjayPGX uGXEKNkqJXaJ wUF5KXhezBW4 L649JAT25GtE
+jDURoVqKDTEb ELf4a2UMVOIs crj7XAA62iMf 21PuHjtbSz3T oMt0qZrsgym2
+YKUNTwSIkx4R EkSlJI3YllVz adSez36mDOZk 9ISj4J1wUHHt dzfoI2OI3uKp
+mDBNhpXgVEpM dd9XIDfM1iYy 0C5ZWEf89GKf TwlE1yh4AdP9 HSIx1xUYXONK
+hSFfpdi7zsBp OGheJUbgCJCq 5meS1Ss6AVaN YhKmxbzbHGgg oflQON8JZbrN
---
+++ pwg.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+todo: 
+input arg for length of password
+password with just numbers or just letters or both
+*/
+
+int main() {
+    int begLength = 0;
+    int endLength = 62;
+    int spaceNum = 0;
+
+    char charset[62] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 
+        'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', 
+        '6', '7', '8', '9', '0'};
+
+    srand(time(0)); 
+    for(int numConOne = 0; numConOne < 30; numConOne++) {
+        for(int numCon = 0; numCon < 12; numCon++) {
+            int randomIndex = (rand() % (endLength - begLength));
+            printf("%c", charset[randomIndex]);
+        }
+
+        if(spaceNum == 4) {
+            printf("\n");
+            spaceNum = 0;
+        } else {
+            printf(" ");
+            spaceNum++;
+        } 
+    }
+}
