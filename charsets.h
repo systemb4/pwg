0c7d2dafcaf7bc32b7937b8e383662c41324dbca
systemb4 <lukasnitzsche@yahoo.com>
2021-02-20 21:34:11 -0600

    clean charsets and config

--- charsets.h
+++ charsets.h
@@ -2,6 +2,7 @@
    charsets.h
 */
 
+int numcount = 0;
 char charset[62] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 
     'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /*
+   config.h
    edit this file to conform to your preferred defaults
 */
 
 int pwlength = 10; // number of characters in password 
-int passwordAmount = 75; // amount of passwords in default
+int passwordAmount = 75; // amount of passwords in default, if 1 then will be checked for 'minNum'
 int minNum = 3; // minimum amount of numbers in password
-int numcount = 0; // need for number counter in main
